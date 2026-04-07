49d48ed721397b1a2ac566529fd9a3e4a8b5d396
noh1es1tate <songyk@buaa.edu.cn>
2026-04-07 15:56:43 +0800

    modify backend port

--- g-seeker-backend/cmd/server/main.go
+++ g-seeker-backend/cmd/server/main.go
@@ -2,18 +2,31 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 
 	"g-seeker-backend/internal/router"
 )
 
 func main() {
+	// Load .env file
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Printf("Warning: Error loading .env file: %v", err)
+	}
+
 	r := gin.Default()
 
 	router.RegisterRoutes(r)
 
-	addr := ":6657"
+	// Get port from environment variable, default to 3000
+	port := os.Getenv("BACKEND_PORT")
+	if port == "" {
+		port = "3000"
+	}
+	addr := ":" + port
 	log.Printf("server is running at %s", addr)
 
 	if err := r.Run(addr); err != nil {
